a6494d309726f464d360ad8fec7a4ad9c3b5a7d2
ranveer <sanskar9797@gmail.com>
2023-12-01 20:10:19 +0530

    gg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 import App from './App';
 import { ThemeProvider } from "@material-tailwind/react";
 
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 	  <ThemeProvider>
-		  <App />
+		  <BrowserRouter basename={process.env.PUBLIC_URL}>
+			  <App />
+		  </BrowserRouter>
 	  </ThemeProvider>
   </React.StrictMode>
 );
